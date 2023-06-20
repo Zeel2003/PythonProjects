938397cfbe0546427c307804657c89f35bb4e514
Zeel <74505506+Zeel2003@users.noreply.github.com>
2023-06-20 12:17:50 -0400

    Update SimpleInterestCalculator.py

--- SimpleInterestCalculator.py
+++ SimpleInterestCalculator.py
@@ -10,16 +10,19 @@ class SimpleInterestCalculator:
 
 # Given values
 
-# calculator = SimpleInterestCalculator(1000,2.5,10)
+# calculator = SimpleInterestCalculator(1000,4.1,10)
 # interest = calculator.calculate_simple_interest()
-# print(interest)
+# print('Your investment of 1000 for 10 years for 2.5 percentage interest will give you total return of',
+#       interest + 1000)
 
-# user input
+
 
 pAmount = float(input('Please enter principle amount: '))
 interestRate = float(input('Please enter interest rate: '))
-time = float(input('Please enter time period: '))
+time = float(input('Please enter time period in years: '))
 
 calculator = SimpleInterestCalculator(pAmount, interestRate,time)
 interest = calculator.calculate_simple_interest()
-print(interest)
+print('Your investment of',pAmount,'for',time,'years, with',interestRate,
+      'interest rate will give you total return of',interest + pAmount,'after',time,'years.')
+
