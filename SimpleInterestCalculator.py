86b208f9695ca6f8e74d98372f345cfb5cb47dc2
Zeel <74505506+Zeel2003@users.noreply.github.com>
2023-06-20 00:17:40 -0400

    Update SimpleInterestCalculator.py

--- SimpleInterestCalculator.py
+++ SimpleInterestCalculator.py
@@ -1,7 +1,3 @@
-# Create a Python class called SimpleInterestCalculator. The class should have a constructor 
-# that takes the principal amount, interest rate, and time period as parameters. Implement a method 
-# called calculate_simple_interest that calculates and returns the simple interest.
-
 class SimpleInterestCalculator:
     def __init__(self,pAmount,interestRate,time):
         self.pAmount = pAmount
